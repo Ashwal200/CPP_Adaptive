29bfcd296ca12fc39bd394cacbf1de537b80e462
Ashwal200 <Ashwal200@gmail.com>
2023-03-22 12:58:25 +0200

    Done

--- AdptArray.c
+++ AdptArray.c
@@ -14,20 +14,26 @@ typedef struct AdptArray_
 
 PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC del, PRINT_FUNC print_f)
 {
+    // alloc the struct
     PAdptArray new_AdptArray = (PAdptArray)malloc(sizeof(AdptArray));
+    // alloc the array
     new_AdptArray->array = (PElement *)calloc(1, sizeof(PElement));
+    // define the three funcrions
     new_AdptArray->copy_func = copy;
     new_AdptArray->del_func = del;
     new_AdptArray->print_func = print_f;
-    new_AdptArray->size = 0;
+    // define the size
+    new_AdptArray->size = 1;
     return new_AdptArray;
 };
 void DeleteAdptArray(PAdptArray padptArray)
 {
+    // if the struct undefine return
     if (padptArray == NULL)
     {
         return;
     }
+    // delete first all the array that are not null
     for (int i = 0; i < padptArray->size; i++)
     {
         if (padptArray->array[i] != NULL)
@@ -35,17 +41,20 @@ void DeleteAdptArray(PAdptArray padptArray)
             padptArray->del_func(padptArray->array[i]);
         }
     }
+    // free the array
     free(padptArray->array);
+    // free the struct
     free(padptArray);
 }
 
 Result SetAdptArrayAt(PAdptArray padptArray, int index, PElement pelement)
 {
+    // if the struct null return fail
     if (padptArray == NULL)
     {
         return FAIL;
     }
-
+    // if the index is negetive return fail
     if (index < 0)
     {
         return FAIL;
@@ -65,6 +74,8 @@ Result SetAdptArrayAt(PAdptArray padptArray, int index, PElement pelement)
     }
     // if the size is smaller from the index we will make
     // new array that all the elements are NULL
+    // copy the element in the old array free them
+    // put the new element
     else if (index >= padptArray->size)
     {
         PElement *new_array = (PElement *)calloc(index + 1, sizeof(PElement));
@@ -90,6 +101,7 @@ Result SetAdptArrayAt(PAdptArray padptArray, int index, PElement pelement)
         padptArray->array[index] = padptArray->copy_func(pelement);
         return SUCCESS;
     }
+    // defultivy
     return FAIL;
 }
 
@@ -109,7 +121,7 @@ PElement GetAdptArrayAt(PAdptArray padptArray, int index)
     {
         return NULL;
     }
-
+    // copy the element and return the element
     PElement answer = padptArray->copy_func(padptArray->array[index]);
     return answer;
 }
@@ -130,7 +142,7 @@ void PrintDB(PAdptArray padptArray)
     {
         return;
     }
-
+    // print any element that isn't null with his own print function
     for (int i = 0; i < padptArray->size; i++)
     {
         if (padptArray->array[i] != NULL)
